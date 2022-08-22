d9aea9b81d31c96d22e6a5a465b2c066679eadb9
lucasgdb <lucasgdbittencourt@gmail.com>
2022-08-22 16:28:14 -0300

    refactor: remove comment

--- src/utils/getRandomNumber.ts
+++ src/utils/getRandomNumber.ts
@@ -4,6 +4,5 @@ interface getRandomNumberProps {
 }
 
 export default function getRandomNumber({ min, max }: getRandomNumberProps = { min: 1, max: 20 }) {
-  // between 1 and 20
   return Math.floor(Math.random() * max) + min;
 }
