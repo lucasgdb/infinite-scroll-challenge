dd85476d76bb976d4d3fe7612ee3ac2528f30ebc
lucasgdb <lucasgdbittencourt@gmail.com>
2022-08-22 23:03:00 -0300

    refactor: add box shadow

--- src/components/ScrollableFloatingCard.tsx
+++ src/components/ScrollableFloatingCard.tsx
@@ -18,7 +18,7 @@ export default function ScrollableFloatingCard() {
 
   return (
     <section
-      className="fixed top-[16px] right-[16px] w-[300px] h-[300px] border rounded-[8px] overflow-y-auto p-4 bg-white flex flex-col gap-[16px] overscroll-contain customized-scroll"
+      className="fixed top-[16px] right-[16px] w-[300px] h-[300px] border shadow-2xl rounded-[8px] overflow-y-auto p-4 bg-white flex flex-col gap-[16px] overscroll-contain customized-scroll"
       onScroll={handleScroll}
     >
       {articles.map((article) => (
